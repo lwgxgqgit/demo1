601575e21cc78c712486bb42d9d92128c9b4032b
lwgxgqgit <15983316702@163.com>
2019-12-11 16:23:26 +0800

    demo1--first--commit

---
+++ demo1/src/main/java/com/hiekn/demo/App.java
@@ -0,0 +1,13 @@
+package com.hiekn.demo;
+
+/**
+ * @author lwg
+ * @Date: 2019/12/11 16:21
+ */
+public class App {
+    public static void main(String[] args) {
+
+
+        System.out.println("demo1 --- App----");
+    }
+}
